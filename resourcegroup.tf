98e127601115aeb2344c137663e601c6cc2ae267
antonysijin <antonysijin@gmail.com>
2026-02-20 13:08:23 +0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "sij-rg2" {
   name     = "sij-rg2"
   location = "westus"
   }
+
+
+resource "azurerm_resource_group" "sij-rg3" {
+  name     = "sij-rg3"
+  location = "westus"
+  }
